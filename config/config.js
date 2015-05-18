8e1d5c5ef6040a182b6c1e52a56d26811ed8d345
joeireland <joe.ireland@gmail.com>
2015-05-17 20:25:44 -0400

    Added configuration panel

---
+++ config/config.js
@@ -0,0 +1,160 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) Sun May 17 2015 Joe Ireland
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORTOR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
+
+/**
+ * Copyright Configuration
+ */
+define(function (require, exports, module) {
+    "use strict";
+
+    var CONST = {
+            PANEL_ID:         "joeireland-copyright-panel",
+            BUTTON_ID:        "joeireland-copyright-btn",
+            CLOSE_ID:         "joeireland-copyright-close",
+            SAVE_ID:          "joeireland-copyright-save",
+            AUTHOR_ID:        "joeireland-copyright-author",
+            COMMENT_CMD_ID:   "joeireland-copyright-commentCmd",
+            TEXT_ID:          "joeireland-copyright-text",
+            DATE_VAR:         "${DATE}",
+            AUTHOR_VAR:       "${AUTHOR}",
+            AUTHOR_PREF:      "author",
+            COMMENT_CMD_PREF: "commentCmd",
+            TEXT_PREF:        "text",
+            DEFAULT_TEXT:     ["The MIT License (MIT)",
+                               "",
+                               "Copyright (c) ${DATE} ${AUTHOR}",
+                               "",
+                               "Permission is hereby granted, free of charge, to any person obtaining a copy of",
+                               "this software and associated documentation files (the \"Software\"), to deal in",
+                               "the Software without restriction, including without limitation the rights to",
+                               "use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of",
+                               "the Software, and to permit persons to whom the Software is furnished to do so,",
+                               "subject to the following conditions:",
+                               "",
+                               "The above copyright notice and this permission notice shall be included in all",
+                               "copies or substantial portions of the Software.",
+                               "",
+                               "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
+                               "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS",
+                               "FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR",
+                               "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER",
+                               "IN AN ACTION OF CONTRACT, TORTOR OTHERWISE, ARISING FROM, OUT OF OR IN",
+                               "CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.",
+                               ""]
+        },
+        WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
+        ExtensionUtils   = brackets.getModule("utils/ExtensionUtils"),
+        Resizer          = brackets.getModule("utils/Resizer"),
+        PrefManager      = brackets.getModule("preferences/PreferencesManager"),
+        Preferences      = PrefManager.getExtensionPrefs("joeireland.copyright"),
+        Panel            = require("text!./html/panel.html"),
+        Button           = require("text!./html/button.html"),
+        $button,
+        $panel;
+
+    /**
+    * Constructor to create Copyright Config manager.
+    *
+    * @constructor
+    */
+    function Config() {
+        ExtensionUtils.loadStyleSheet(module, "css/main.less");
+
+        $("#main-toolbar .buttons").append(Button);
+        $button = $("#" + CONST.BUTTON_ID).on("click", this.togglePanel.bind(this));
+
+        WorkspaceManager.createBottomPanel(CONST.PANEL_ID, $(Panel), 600);
+        $panel = $("#" + CONST.PANEL_ID);
+        $("#" + CONST.CLOSE_ID).on("click", this.togglePanel.bind(this));
+        $("#" + CONST.SAVE_ID).on("click", this.save.bind(this));
+    }
+
+    Config.prototype.get = function () {
+        var author  = Preferences.get(CONST.AUTHOR_PREF),
+            command = Preferences.get(CONST.COMMENT_CMD_PREF),
+            lines   = Preferences.get(CONST.TEXT_PREF);
+
+        if (!author || this.isTextUnset(lines)) {
+            return {status: "not-configured"};
+        } else if (!command) {
+            return {status: "no-command"};
+        }
+
+        var today  = new Date().toDateString(),
+            length = lines.length - 1,
+            width  = lines[length].length,
+            text   = lines.join("\n")
+                          .replace(CONST.DATE_VAR, today)
+                          .replace(CONST.AUTHOR_VAR, author);
+
+        return { text: text, command: command, length: length, width: width, status: "ok" };
+    };
+
+    Config.prototype.save = function () {
+        Preferences.set(CONST.AUTHOR_PREF, $("#" + CONST.AUTHOR_ID).val());
+        Preferences.set(CONST.COMMENT_CMD_PREF, $("#" + CONST.COMMENT_CMD_ID).val());
+        Preferences.set(CONST.TEXT_PREF, $("#" + CONST.TEXT_ID).val().split("\n"));
+        this.togglePanel();
+    };
+
+    Config.prototype.update = function () {
+        var command = Preferences.get(CONST.COMMENT_CMD_PREF);
+        var text    = Preferences.get(CONST.TEXT_PREF);
+
+        if (!command) {
+            command = "edit.blockComment";
+        }
+
+        if (this.isTextUnset(text)) {
+            text = CONST.DEFAULT_TEXT;
+        }
+
+        $("#" + CONST.AUTHOR_ID).val(Preferences.get(CONST.AUTHOR_PREF));
+        $("#" + CONST.COMMENT_CMD_ID).val(command);
+        $("#" + CONST.TEXT_ID).val(text.join("\n"));
+    };
+
+    Config.prototype.showPanel = function () {
+        if (!$button.hasClass("active")) {
+            this.togglePanel();
+        }
+    };
+
+    Config.prototype.togglePanel = function () {
+        Resizer.toggle($panel);
+        $button.toggleClass("active");
+
+        if ($button.hasClass("active")) {
+            this.update();
+        }
+    };
+
+    Config.prototype.isTextUnset = function (text) {
+        return !text || text.length === 0 || (text.length === 1 && text[0].length === 0);
+    };
+
+    module.exports = Config;
+});
--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-/*---------------------------------------------------------------------------------
-* The MIT License (MIT)
-* 
-* Copyright (c) Tue May 12 2015 Joe Ireland
-* 
-* Permission is hereby granted,free of charge, to any person obtaining a copy of
-* this software and associated documentation files (the "Software"), to deal in
-* the Software without restriction, includingwithout limitation the rights to
-* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-* the Software, and to permit persons to whom the Software isfurnished to do so,
-* subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of theSoftware.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-* FOR APARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-* IN AN ACTION OF CONTRACT, TORTOR OTHERWISE, ARISING FROM, OUT OF OR IN
-* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*-------------------------------------------------------------------------------*/
+/*
+The MIT License (MIT)
+
+Copyright (c) Sun May 17 2015 Joe Ireland
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORTOR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, brackets */
+/*global define, brackets, $ */
 
 /**
  * A Brackets extension to insert copyright notices at the top of documents
@@ -30,63 +30,52 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var CommandManager = brackets.getModule("command/CommandManager"),
+    var CONST = {
+            COMMAND_ID:   "joeireland.copyright",
+            COMMAND_NAME: "Insert Copyright",
+            DIALOG_TITLE: "Copyright Extension Configuration Error",
+            NOT_CFG_MSG:  "You must configure the Copyright Extension before using it.",
+            OLD_CFG_MSG:  "The Copyright extension now comments your text for you. " +
+                          "You must remove all commenting characters before using it.",
+            DATE_VAR:     "${DATE}",
+            AUTHOR_VAR:   "${AUTHOR}",
+            HOTKEY:       "Alt-C"
+        },
+        CommandManager = brackets.getModule("command/CommandManager"),
         EditorManager  = brackets.getModule("editor/EditorManager"),
         Menus          = brackets.getModule("command/Menus"),
         Dialogs        = brackets.getModule("widgets/Dialogs"),
         DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
-        PrefManager    = brackets.getModule("preferences/PreferencesManager"),
-        Preferences    = PrefManager.getExtensionPrefs("joeireland.copyright"),
-        COMMAND_ID     = "joeireland.copyright",
-        COMMAND_NAME   = "Insert Copyright",
-        HOTKEY         = "Alt-C";
+        Config         = require("config/config"),
+        config         = new Config();
 
     function copyright() {
-        var text  = Preferences.get("text");
-        var author = Preferences.get("author");
+        var data = config.get();
         
-        if (!author || !text || text.length === 0) {
-            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, "Copyright Extension Configuration Error",
-                                    'Select "Debug > Open Preferences File" and set the following configuration variables:<br><pre>' +
-                                    '  "joeireland.copyright.author":     "author of the copyright"<br>' +
-                                    '  "joeireland.copyright.text":       "The copyright text (uncommented)"<br>' +
-                                    '  "joeireland.copyright.commentCmd": "edit.blockComment" or "edit.lineComment"</pre>');
+        switch (data.status) {
+        case "not-configured":
+            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, CONST.DIALOG_TITLE, CONST.NOT_CFG_MSG);
+            config.showPanel();
             return;
-        }
-        
-        var commentCmd = Preferences.get("commentCmd");
-
-        if (!commentCmd) {
-            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, "Copyright Extension Configuration Error",
-                                    'The Copyright extension now comments your copyright text for you. You must remove ' +
-                                    'your comment characters from the copyright text for this version of the extension<hr>' +
-                                    'Select "Debug > Open Preferences File" and set the following configuration variables:<br><pre>' +
-                                    '  "joeireland.copyright.author":     "author of the copyright"<br>' +
-                                    '  "joeireland.copyright.text":       "The copyright text (uncommented)"<br>' +
-                                    '  "joeireland.copyright.commentCmd": "edit.blockComment" or "edit.lineComment"</pre>');
+        case "no-command": // Backward compatibility for v1.0.1 which didn't use Brackets commenting commands.
+            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, CONST.DIALOG_TITLE, CONST.OLD_CFG_MSG);
+            config.showPanel();
             return;
         }
 
         var editor = EditorManager.getFocusedEditor();
         
         if (editor) {
-            var today    = new Date().toDateString(),
-                toInsert = "",
-                i;
-            
-            for (i = 0; i < text.length; i++) {
-                toInsert += text[i].replace("${DATE}", today).replace("${AUTHOR}", author) + "\n";
-            }
-            
-            editor.document.replaceRange(toInsert, {line: 0, ch: 0});
-            editor.setSelection({line: 0, ch: 0}, {line: text.length - 1, ch: text[text.length - 1].length}, true);
-            CommandManager.execute(commentCmd);
-            editor.setCursorPos({line: 0, ch: 0});
+            editor.document.replaceRange(data.text + "\n", {line: 0, ch: 0});
+            editor.setSelection({line: 0, ch: 0}, {line: data.length, ch: data.width}, true);
+            CommandManager.execute(data.command).done(function () {
+                editor.setCursorPos({line: 0, ch: 0});
+            });
         }
     }
     
     var editMenu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
     
-    CommandManager.register(COMMAND_NAME, COMMAND_ID, copyright);
-    editMenu.addMenuItem(COMMAND_ID, HOTKEY);
+    CommandManager.register(CONST.COMMAND_NAME, CONST.COMMAND_ID, copyright);
+    editMenu.addMenuItem(CONST.COMMAND_ID, CONST.HOTKEY);
 });
